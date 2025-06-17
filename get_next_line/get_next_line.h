c7e3873cc202c93ce6e8b22edece57717f90c370
Pedro Castro Gomez <pecastro@c3a3c10.42berlin.de>
2025-06-17 09:54:10 +0200

    new structured approach based on cup and basin metaphor by Lannur blog. This version uses BUFFER_SIZE as nbytes and buffer size in read(). Currently in debugging phase

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -1,6 +1,7 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+# include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
 
@@ -9,6 +10,12 @@
 #endif
 
 char	*get_next_line(int fd);
-char	*new_str(char *buffer, char *str, int i, int reset_byte_count);
+char	*read_file(char *storage_buffer, int fd);
+char	*join_buffers(char *storage_buffer, char *read_buffer);
+char	*cut_newline(char *storage_buffer);
+char	*update_storage_buffer(char	*storage_buffer);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char const *s1, char const *s2);
+size_t	ft_strlen(const char *s);
 
 #endif
--- get_next_line/get_next_line_utils.c
+++ get_next_line/get_next_line_utils.c
@@ -1,49 +1,146 @@
 #include "get_next_line.h"
-#include <stdio.h>
 
-char	*new_str(char *buffer, char *str, int i, int reset_byte_count)
+size_t	ft_strlen(const char *s)
 {
-	char		*new_str;
-	int			j;
-	int			k;
-	static int	byte_count = 0;
+	size_t	i;
 
-	if (reset_byte_count > 0)
+	i = 0;
+	while (s[i] != '\0')
 	{
-		byte_count = 0;
-		return (NULL);
+		i ++;
 	}
-	byte_count = byte_count + i + 1;
-	printf("new_str: ********************byte_count is %i\n", byte_count);
-	new_str = malloc(sizeof(char) * byte_count + 1);
-	if (new_str == NULL)
+	return (i);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char			*ptr;
+	unsigned int	i;
+	unsigned int	j;
+
+	if (!s1 || !s2)
 		return (NULL);
-	j = 0;
-	while (j <= byte_count)
+	ptr = malloc(sizeof(char) * ((ft_strlen((const char *)s1))
+				+ ft_strlen((const char *)s2) + 1));
+	if (!ptr)
+		return (NULL);
+	i = 0;
+	while (s1[i])
 	{
-		new_str[j] = '\0';
+		ptr[i] = s1[i];
+		i ++;
+	}
+	j = 0;
+	while (s2[j])
+	{
+		ptr[i + j] = s2[j];
 		j ++;
 	}
-	j = 0;
-	if (str != NULL)
+	ptr[i + j] = '\0';
+	return (ptr);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
 	{
-		while (str[j])
+		if ((unsigned char)s[i] == (unsigned char)c)
 		{
-			new_str[j] = str[j];
-			j ++;
+			return (((char *)s) + i);
 		}
-		printf("new_str: when copying str into new_str, str = %s\n", str);
-		printf("new_str: when copying str into new_str, new_str = %s\n", new_str);
-		free (str);
+		i ++;
 	}
-	k = 0;
-	printf("new_str: when copying buffer into new_str, buffer = %s\n", buffer);
-	while (buffer[k])
+	if ((char)c == '\0')
 	{
-		new_str[j + k] = buffer[k];
-		printf("new_str: when copying buffer into new_str, new_str[%i + %i] = %c\n", j, k, new_str[j + k]);
-		k ++;
+		return (((char *)s) + i);
 	}
-	printf("new_str: complete new string after copying str + buffer, new_str = %s\n", new_str);
-	return (new_str);
+	return (0);
+}
+
+char	*update_storage_buffer(char	*storage_buffer)
+{
+	char	*position_newline;
+	size_t	updated_length;
+	char	*updated_storage_buffer;
+	int		i;
+
+	position_newline = ft_strchr(storage_buffer, '\n');
+	updated_length = ft_strlen(position_newline + 1);
+	updated_storage_buffer = malloc(sizeof(char) * (updated_length + 1));
+	i = 0;
+	while ((position_newline + 1)[i])
+	{
+		updated_storage_buffer[i] = (position_newline + 1)[i];
+		i ++;
+	}
+	updated_storage_buffer[i] = '\0';
+	return (updated_storage_buffer);
+}
+
+char	*cut_newline(char *storage_buffer)
+{
+	char	*position_newline;
+	size_t	newline_length;
+	char	*newline;
+	int		i;
+
+	position_newline = ft_strchr(storage_buffer, '\n');
+	newline_length = (ft_strlen(storage_buffer) - ft_strlen(position_newline + 1));
+	newline = malloc(sizeof(char) * (newline_length + 1));
+	i = 0;
+	while (storage_buffer[i] && storage_buffer[i] != '\n')
+	{
+		newline[i] = storage_buffer[i];
+		i ++;
+	}
+	newline[i] = '\0';
+	return (newline);
+}
+
+char	*join_buffers(char *storage_buffer, char *read_buffer)
+{
+	printf("join_buffers: function call works\n");
+	if (!storage_buffer)
+	{
+		printf("join_buffers: !storage_buffer, create empty buffer size 1\n");
+		storage_buffer = malloc(sizeof(char) * 1);
+		storage_buffer[0] = '\0';
+	}
+	printf("join_buffers: ft_strjoin is called and value stored in storage_buffer\n");
+	storage_buffer = ft_strjoin(storage_buffer, read_buffer);
+	printf("join_buffers: storage_buffer = %s\n", storage_buffer);
+	return (storage_buffer);
+}
+
+char	*read_file(char *storage_buffer, int fd)
+{
+	char	*read_buffer;
+	int		bytes_read;
+
+	printf("read_file: function call works\n");
+	read_buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!read_buffer)
+	{
+		printf("read_file: !read buffer, return(NULL)\n");
+		return (NULL);
+	}
+	bytes_read = 1;
+	printf("read file: bytes_read initialized to 1\n");
+	while (bytes_read > 0)
+	{
+		bytes_read = read(fd, read_buffer, BUFFER_SIZE);
+		if (bytes_read == -1)
+		{
+			free(read_buffer);
+			return (NULL);
+		}
+		printf("read_file: join_buffers is called and value stored in storage_buffer\n");
+		storage_buffer = join_buffers(storage_buffer, read_buffer);
+		if (ft_strchr(storage_buffer, '\n') || !storage_buffer)
+			break;
+	}
+	return (storage_buffer);
 }
