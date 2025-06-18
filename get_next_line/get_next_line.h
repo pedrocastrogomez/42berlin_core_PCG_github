259d2506484581bda5ebad05651834dbd566adb3
Pedro Castro Gomez <pecastro@c4b7c3.42berlin.de>
2025-06-18 20:06:56 +0200

    improved structure based on basin&cup article. Great exercise reading and understanding the code in the article. No leaks in valgrind, but paco still shows some sigsev I have to fix.

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -12,10 +12,11 @@
 char	*get_next_line(int fd);
 char	*read_file(char *storage_buffer, int fd);
 char	*join_buffers(char *storage_buffer, char *read_buffer);
-char	*cut_newline(char *storage_buffer);
-char	*update_storage_buffer(char	*storage_buffer);
+char	*cut_line(char *storage_buffer);
+char	*update_storage(char *storage_buffer);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strjoin(char const *s1, char const *s2);
 size_t	ft_strlen(const char *s);
+void	*ft_malloc(size_t nmemb, size_t size);
 
 #endif
--- get_next_line/get_next_line_utils.c
+++ get_next_line/get_next_line_utils.c
@@ -6,13 +6,40 @@ size_t	ft_strlen(const char *s)
 
 	i = 0;
 	while (s[i] != '\0')
-	{
 		i ++;
-	}
 	return (i);
 }
 
-char	*ft_strjoin(char const *s1, char const *s2)
+void	*ft_malloc(size_t nmemb, size_t size)
+{
+	char	*ptr;
+
+	if (nmemb == 0 || size == 0)
+		return (malloc(0));
+	ptr = malloc(nmemb * size);
+	if (!ptr)
+		return (NULL);
+	ptr[nmemb - 1] = '\0';
+	return (ptr);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+	{
+		if ((unsigned char)s[i] == (unsigned char)c)
+			return (((char *)s) + i);
+		i ++;
+	}
+	if ((char)c == '\0')
+		return (((char *)s) + i);
+	return (0);
+}
+
+char	*ft_strjoin(const char *s1, const char *s2)
 {
 	char			*ptr;
 	unsigned int	i;
@@ -21,7 +48,7 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	if (!s1 || !s2)
 		return (NULL);
 	ptr = malloc(sizeof(char) * ((ft_strlen((const char *)s1))
-				+ ft_strlen((const char *)s2) + 1));
+			  + ft_strlen((const char *)s2) + 1));
 	if (!ptr)
 		return (NULL);
 	i = 0;
@@ -40,115 +67,62 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	return (ptr);
 }
 
-char	*ft_strchr(const char *s, int c)
+char	*update_storage(char *storage_buffer)
 {
-	int	i;
-
-	i = 0;
-	while (s[i])
-	{
-		if ((unsigned char)s[i] == (unsigned char)c)
-		{
-			return (((char *)s) + i);
-		}
-		i ++;
-	}
-	if ((char)c == '\0')
-	{
-		return (((char *)s) + i);
-	}
-	return (0);
-}
-
-char	*update_storage_buffer(char	*storage_buffer)
-{
-	char	*position_newline;
-	size_t	updated_length;
+	char	*newline_pos;
 	char	*updated_storage_buffer;
+	int		len_newline_pos;
 	int		i;
 
-	position_newline = ft_strchr(storage_buffer, '\n');
-	if (position_newline == NULL)
-		return (NULL);
-	updated_length = ft_strlen(position_newline + 1);
-	if (updated_length == 0)
-	{
-		updated_storage_buffer = malloc(sizeof(char) * 1);
-		if (!updated_storage_buffer)
-			return(NULL);
-		updated_storage_buffer[0] = '\0';
-		return (updated_storage_buffer);
-	}
-	updated_storage_buffer = malloc(sizeof(char) * (updated_length + 1));
-	if (!updated_storage_buffer)
-		return (NULL);
+	if (!storage_buffer || *storage_buffer == '\0')
+		return (free(storage_buffer), NULL);
+	newline_pos = ft_strchr(storage_buffer, '\n');
+	if (!newline_pos) //makes sense: if no newline is found why bother updating? i.e. EOF
+		return (free(storage_buffer), NULL);
+	len_newline_pos = ft_strlen((const char *)newline_pos + 1);
+	updated_storage_buffer = ft_malloc(len_newline_pos + 1, sizeof(updated_storage_buffer));
 	i = 0;
-	while ((position_newline + 1)[i])
+	while (i < len_newline_pos)
 	{
-		updated_storage_buffer[i] = (position_newline + 1)[i];
+		updated_storage_buffer[i] = (newline_pos + 1)[i];
 		i ++;
 	}
-	updated_storage_buffer[i] = '\0';
+	// updated_storage_buffer is already null terminated in ft_malloc.
+	free (storage_buffer);
 	return (updated_storage_buffer);
 }
 
-char	*cut_newline(char *storage_buffer)
+char	*cut_line(char *storage_buffer)
 {
-	char	*position_newline;
-	size_t	newline_length;
-	char	*newline;
+	char	*line;
+	int		len;
 	int		i;
 
-	if (storage_buffer == NULL)
-		return (NULL);
-	position_newline = ft_strchr(storage_buffer, '\n');
-	if (position_newline != NULL)
-	{
-		newline_length = (ft_strlen(storage_buffer) - ft_strlen(position_newline + 1));
-		newline = malloc(sizeof(char) * (newline_length + 1));
-	}
-	else
-	{
-		newline = malloc(sizeof(char) * (ft_strlen(storage_buffer) + 1));
-	}
-	if (!newline)
+	if (!storage_buffer || *storage_buffer == '\0')
 		return (NULL);
 	i = 0;
 	while (storage_buffer[i] && storage_buffer[i] != '\n')
-	{
-		newline[i] = storage_buffer[i];
 		i ++;
-	}
+	len = i;
 	if (storage_buffer[i] == '\n')
-		newline[i++] = '\n';
-	newline[i] = '\0';
-	return (newline);
+		len ++;
+	line = ft_malloc(len + 1, sizeof(*line));
+	i = 0;
+	while (i < len)
+	{
+		line[i] = storage_buffer[i];
+		i ++;
+	} //line is already null terminated with ft_malloc.
+	return (line);
 }
 
 char	*join_buffers(char *storage_buffer, char *read_buffer)
 {
-	char	*new_storage_buffer;
-	char	*temp;
+	char	*joint_buffer;
 
-	if (!storage_buffer)
-	{
-		temp = malloc(sizeof(char) * 1);
-		if (temp == NULL)
-			return (NULL);
-		temp[0] = '\0';
-	}
-	else
-	{
-		temp = storage_buffer;
-	}
-	new_storage_buffer = ft_strjoin(temp, read_buffer);
-	if (new_storage_buffer == NULL)
-	{
-		free(temp);
-		return (NULL);
-	}
-	free(temp);
-	return (new_storage_buffer);
+	joint_buffer = ft_strjoin(storage_buffer, read_buffer);
+	free (storage_buffer);
+	return (joint_buffer);
 }
 
 char	*read_file(char *storage_buffer, int fd)
@@ -156,35 +130,24 @@ char	*read_file(char *storage_buffer, int fd)
 	char	*read_buffer;
 	int		bytes_read;
 
-	read_buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
-	if (!read_buffer)
-	{
+	if (!storage_buffer)
 		return (NULL);
-	}
 	bytes_read = 1;
 	while (bytes_read > 0)
 	{
+		read_buffer = ft_malloc(BUFFER_SIZE + 1, sizeof(*read_buffer));
+		if (!read_buffer)
+			return (NULL);
 		bytes_read = read(fd, read_buffer, BUFFER_SIZE);
-		if (bytes_read == -1)
-		{
-			free(read_buffer);
-			return (NULL);
-		}
-		read_buffer[bytes_read] = '\0';
-		if (bytes_read == 0 && (storage_buffer == NULL || *storage_buffer == '\0'))
-		{
-			free(read_buffer);
-			return (NULL);
-		}
 		storage_buffer = join_buffers(storage_buffer, read_buffer);
-		if (storage_buffer == NULL)
-		{
-			free(read_buffer);
+		if (!storage_buffer)
 			return (NULL);
-		}
-		if (ft_strchr(storage_buffer, '\n') || bytes_read == 0)
+		if (ft_strchr(storage_buffer, '\n'))
+	  	{
+	  		free (read_buffer);
 			break;
+	  	}
+	  	free(read_buffer);
 	}
-	free (read_buffer);
 	return (storage_buffer);
 }
