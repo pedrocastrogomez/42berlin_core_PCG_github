0607be85235e448f5aa9cd43c9b80ab5a83690c8
Pedro Castro Gomez <pecastro@c4b7c5.42berlin.de>
2025-06-19 12:53:50 +0200

    added guard clauses in get_next_line to fix double free and sigsev

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -7,29 +7,13 @@ char	*get_next_line(int fd)
 
 	if (fd < 0 || read(fd, NULL, 0) < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	if (!storage_buffer) //what would happen if NULL was returned instead?
-		storage_buffer = ft_malloc(1, sizeof(*storage_buffer));
 	if (!storage_buffer)
-		return (NULL);
-	storage_buffer = read_file(storage_buffer, fd);
-	if (!ft_strchr(storage_buffer, '\n')) //really?
+		storage_buffer = ft_calloc(1, sizeof(*storage_buffer));
+	if (!ft_strchr(storage_buffer, '\n'))
+		storage_buffer = read_file(storage_buffer, fd);
+	if (!storage_buffer)
 		return (free(storage_buffer), NULL);
-	line = cut_line(storage_buffer); //control if (!line) ?
-	storage_buffer = update_storage(storage_buffer); //ownership? free where?
+	line = cut_line(storage_buffer);
+	storage_buffer = update_storage(storage_buffer);
 	return (line);
 }
-
-/*
-*get_next_line; static char *storage_buffer(initialize to 1 byte), char *line; 
-	read_file; char *read_buffer, int bytes_read; ...free(read_buffer)
-		join_buffers; char *temp ...free(storage_buffer)
-			ft_strjoin;
-	cut_line; char *line, int len, int i;
-		ft_strlen;
-		ft_strchr;
-		ft_malloc;
-	update_storage; char *newline_pos, char *updated_storage_buffer, int len_newline_pos, int i; ...free)storage_buffer, return NULL;
-		ft_strlen;
-		ft_strchr;
-		ft_malloc;
-*/
